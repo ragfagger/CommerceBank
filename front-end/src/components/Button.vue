b5006946d9a49e1bc51da9a18c4195bcbb551db8
Chris Lingenfelter <lingenfelter123@gmail.com>
2019-02-05 20:39:10 -0600

    Vue project

---
+++ front-end/src/components/Button.vue
@@ -0,0 +1,59 @@
+<!-- This is a component. Components are reusable items that can be passed props
+-->
+
+<template>
+  <div class="button-container">
+    <button @click="isSelected = !isSelected" :class="{ selected: isSelected }">
+      <!-- These i tags add icons -->
+      <i :class="icon"></i>
+      <!-- br inserts a break. This allows the image and text to display on top
+      of eachother-->
+      <br>
+      {{ topic }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  /* Props are passed to the component when used in App.vue */
+  props: ["topic", "icon"],
+  data() {
+    return {
+      isSelected: false
+    };
+  }
+};
+</script>
+
+<!-- Scoped style only applies to this component -->
+<style scoped>
+button {
+  width: 210px;
+  height: 145px;
+  cursor: pointer;
+  background: rgb(255, 255, 204);
+  font-weight: bold;
+  font-size: 14px;
+  padding: 15px;
+  border: 1px solid rgb(223, 215, 215);
+}
+
+/* Applies when the button is either active or focused */
+button:active,
+button:focus {
+  outline: none;
+}
+
+button:hover {
+  background: rgb(254, 241, 96);
+  transition: background 0.2s ease-in-out;
+}
+
+.selected {
+  background: rgb(4, 147, 114);
+  color: white;
+  border: 1px inset rgb(223, 215, 215);
+}
+</style>
