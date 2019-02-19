19b3a39c7fa28ba33a6f40d67e3243a09d6e2caf
Ebenezer Jeya <ebenezer.jeya@gmail.com>
2019-02-18 20:37:13 -0600

    Add files via upload
    So this contains a bunch of red squiggles aka it doesnt work. Will keep working to fix

---
+++ java/customer/CustomerApplication.java
@@ -0,0 +1,11 @@
+package customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CustomerApplication {
+	public static void main(String... args) {
+		
+        SpringApplication.run(Customer.Application.class, args);
+	}
+}
